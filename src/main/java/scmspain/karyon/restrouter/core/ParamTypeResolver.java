9ea14fbbbf0d5bd2510fb0ab7896f3c9a7c6af38
Alberto <alberto@aramirez.es>
2015-09-10 20:55:10 +0200

    Refactor: improve resolver map creation and population

--- src/main/java/scmspain/karyon/restrouter/core/ParamTypeResolver.java
+++ src/main/java/scmspain/karyon/restrouter/core/ParamTypeResolver.java
@@ -10,25 +10,22 @@ import java.util.function.Function;
 @Singleton
 public class ParamTypeResolver<T> {
 
-  private Map<Class, Function<String, T>> resolver= new HashMap<>();
+  private Map<Class, Function<String, T>> resolver = new HashMap<Class, Function<String, T>>() {{
+    put(int.class, (val) -> (T) Integer.valueOf(val));
+    put(double.class, (val) -> (T) Double.valueOf(val));
+    put(boolean.class, (val) -> (T) Boolean.valueOf(val));
+    put(long.class, (val) -> (T) Long.valueOf(val));
+    put(short.class, (val) -> (T) Short.valueOf(val));
+    put(float.class, (val) -> (T) Float.valueOf(val));
 
-  ParamTypeResolver() {
-
-    resolver.put(int.class, (val) -> (T) Integer.valueOf(val));
-    resolver.put(double.class, (val) -> (T) Double.valueOf(val));
-    resolver.put(boolean.class, (val) -> (T) Boolean.valueOf(val));
-    resolver.put(long.class, (val) -> (T) Long.valueOf(val));
-    resolver.put(short.class, (val) -> (T) Short.valueOf(val));
-    resolver.put(float.class, (val) -> (T) Float.valueOf(val));
-
-    resolver.put(String.class, (val) -> (T) val);
-    resolver.put(Integer.class, (val) -> (T) Integer.valueOf(val));
-    resolver.put(Double.class, (val) -> (T) Double.valueOf(val));
-    resolver.put(Boolean.class, (val) -> (T) Boolean.valueOf(val));
-    resolver.put(Long.class, (val) -> (T) Long.valueOf(val));
-    resolver.put(Short.class, (val) -> (T) Short.valueOf(val));
-    resolver.put(Float.class, (val) -> (T) Float.valueOf(val));
-  }
+    put(String.class, (val) -> (T) val);
+    put(Integer.class, (val) -> (T) Integer.valueOf(val));
+    put(Double.class, (val) -> (T) Double.valueOf(val));
+    put(Boolean.class, (val) -> (T) Boolean.valueOf(val));
+    put(Long.class, (val) -> (T) Long.valueOf(val));
+    put(Short.class, (val) -> (T) Short.valueOf(val));
+    put(Float.class, (val) -> (T) Float.valueOf(val));
+  }};
 
   public T resolveValueType(Class<T> classType, String value){
 
